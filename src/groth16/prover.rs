57eea61b845305279c0f0e411b5539edbad93fda
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-04-11 17:23:46 +0430

    oops

--- src/groth16/prover.rs
+++ src/groth16/prover.rs
@@ -189,7 +189,7 @@ where
     E: Engine,
     C: Circuit<E> + Send,
 {
-    /*let rng = &mut rand::thread_rng();
+    let rng = &mut rand::thread_rng();
     let rnd1 = E::G1::random(rng).into_affine();
     let rnd2 = E::G2::random(rng).into_affine();
     let rnd3 = E::G1::random(rng).into_affine();
@@ -200,7 +200,7 @@ where
             c: rnd3,
         };
         circuits.len()
-    ]);*/
+    ]);
 
     info!("Bellperson {} is being used!", BELLMAN_VERSION);
 
